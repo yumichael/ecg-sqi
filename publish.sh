5d695f4254cdd6dbed78d7bd6ec6e72f47829629
Michael Yu <michael.yang2357@gmail.com>
2016-12-18 22:26:09 +0000

    fix publishing script

--- publish.sh
+++ publish.sh
@@ -3,11 +3,11 @@
 mkdir -p /tmp/workspace
 cp -r * /tmp/workspace/
 git checkout -f -B gh-pages
-git merge master
+git merge master -m "update from master"
 jupyter-nbconvert --to slides explore.ipynb --reveal-prefix=reveal.js
 mv explore.slides.html  index.html
 git add index.html
-git commit -m "update"
+git commit -m "publish update"
 git push origin master gh-pages
 git checkout master
 cp -r /tmp/workspace/* .
