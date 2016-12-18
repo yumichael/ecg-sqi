c5a6e3c1da634e70e5d61034c1dba501228bcf49
Michael Yu <michael.yang2357@gmail.com>
2016-12-18 21:54:54 +0000

    publishing script

--- publish.sh
+++ publish.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-cp -f ../ecg-sqi/explore.ipynb ./
-jupyter-nbconvert --to slides explore.ipynb --reveal-prefix=reveal.js
-mv explore.slides.html  index.html
 mkdir -p /tmp/workspace
 cp -r * /tmp/workspace/
-git add -A .
-git commit -m "Update"
-git checkout -B gh-pages
-cp -r /tmp/workspace/* .
-git add -A .
-git commit -m "new version"
+git checkout -f -B gh-pages
+git merge master
+jupyter-nbconvert --to slides explore.ipynb --reveal-prefix=reveal.js
+mv explore.slides.html  index.html
+git add index.html
+git commit -m "update"
 git push origin master gh-pages
 git checkout master
+cp -r /tmp/workspace/* .
 rm -rf /tmp/workspace
